16ee71acd6b08b381868c9b090e2269b6a59e486
raffael-marinho <silvamarinho10@gmail.com>
2025-07-22 14:43:52 -0300

    feat: ajuste do app para rodar via android studio

---
+++ metro.config.js
@@ -0,0 +1,6 @@
+// Learn more on how to config Metro: https://facebook.github.io/metro/docs/configuration
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
